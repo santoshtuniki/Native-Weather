a39ab34317581b5d1ad36a4fdcabc274e0b83937
santoshtuniki <tunikisantosh123@gmail.com>
2024-05-06 17:04:14 +0530

    Initialize

---
+++ src/screens/AppHeader.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+
+const MyComponent = () => {
+
+    return (
+        <Appbar.Header>
+
+        </Appbar.Header>
+    );
+};
+
+export default MyComponent;
